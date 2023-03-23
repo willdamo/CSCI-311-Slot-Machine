efcdbfcefc17ae806fb4e76e99639d172e72b6c3
willdamo <wdmolina@widener.edu>
2023-03-22 22:14:43 -0400

    Steps 5: replaced the textViews with imageViews and added a restart button- Commit

--- app/src/main/java/com/example/slotmachine/MainActivity.java
+++ app/src/main/java/com/example/slotmachine/MainActivity.java
@@ -2,21 +2,28 @@ package com.example.slotmachine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.media.metrics.Event;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView timeView;
-    TextView timeView2;
-    TextView timeView3;
+    ImageView slotImage;
+    ImageView slotImage2;
+    ImageView slotImage3;
+    Drawable cherry;
+    Drawable grape;
+    Drawable pear;
+    Drawable strawberry;
     Button startButton;
     Button stopButton;
+    Button restartButton;
     int time;
     int time2;
     int time3;
@@ -31,11 +38,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        timeView = findViewById(R.id.timeView);
-        timeView2 = findViewById(R.id.timeView2);
-        timeView3 = findViewById(R.id.timeView3);
+        slotImage = findViewById(R.id.slotImage);
+        slotImage2 = findViewById(R.id.slotImage2);
+        slotImage3 = findViewById(R.id.slotImage3);
+
         startButton = findViewById(R.id.startButton);
         stopButton = findViewById(R.id.stopButton);
+        restartButton = findViewById(R.id.restartButton);
+
+        cherry = getDrawable(R.drawable.cherry);
+        grape = getDrawable(R.drawable.grape);
+        pear = getDrawable(R.drawable.pear);
+        strawberry = getDrawable(R.drawable.strawberry);
 
         time = 0;
         time2 = 0;
@@ -51,8 +65,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 handler.postDelayed(event2, 500);
-                handler.postDelayed(event, 1000);
-                handler.postDelayed(event3, 1500);
+                handler.postDelayed(event, 700);
+                handler.postDelayed(event3, 900);
             }
         });
 
@@ -63,33 +77,58 @@ public class MainActivity extends AppCompatActivity {
                 handler.removeCallbacks(event);
                 handler.removeCallbacks(event3);
 
-                if(time == time2 && time2 == time3){
+                if(slotImage.getDrawable().equals(slotImage2.getDrawable()) && slotImage2.getDrawable().equals(slotImage3.getDrawable())){
                     Toast t = Toast.makeText(getApplicationContext(), "WINNER!!!", Toast.LENGTH_SHORT);
                     t.show();
                 }
+            }
+        });
+
+        restartButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                time = 0;
+                time2 = 0;
+                time3 = 0;
+
+                handler.postDelayed(event2, 500);
+                handler.postDelayed(event, 700);
+                handler.postDelayed(event3, 900);
             }
         });
     }
 
     private class CountEvent implements Runnable{
         public void run(){
-            timeView.setText(time+"");
             if(time==3){
                 time=0;
-            }else{
+            }else if(time==2){
                 time++;
+                slotImage.setImageDrawable(strawberry);
+            }else if(time==1){
+                time++;
+                slotImage.setImageDrawable(pear);
+            }else if(time==0){
+                time++;
+                slotImage.setImageDrawable(grape);
             }
-            handler.postDelayed(event, 1000);
+            handler.postDelayed(event, 250);
         }
     }
 
     private class CountEvent2 implements Runnable{
         public void run(){
-            timeView2.setText(time2+"");
             if(time2==3){
                 time2=0;
-            }else{
+            }else if(time2==2){
                 time2++;
+                slotImage2.setImageDrawable(strawberry);
+            }else if(time2==1){
+                time2++;
+                slotImage2.setImageDrawable(pear);
+            }else if(time2==0){
+                time2++;
+                slotImage2.setImageDrawable(grape);
             }
             handler.postDelayed(event2, 500);
         }
@@ -97,13 +136,21 @@ public class MainActivity extends AppCompatActivity {
 
     private class CountEvent3 implements Runnable{
         public void run(){
-            timeView3.setText(time3+"");
             if(time3==3){
                 time3=0;
-            }else{
+                slotImage3.setImageDrawable(cherry);
+            }else if(time3==2){
                 time3++;
+                slotImage3.setImageDrawable(strawberry);
+            }else if(time3==1){
+                time3++;
+                slotImage3.setImageDrawable(pear);
+            }else if(time3==0){
+                time3++;
+                slotImage3.setImageDrawable(grape);
             }
-            handler.postDelayed(event3, 1500);
+
+            handler.postDelayed(event3, 750);
         }
     }
 }
